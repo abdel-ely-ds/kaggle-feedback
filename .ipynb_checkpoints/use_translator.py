647fcf6c735efdf2d644ecb11c4026defbdbe8f3
Hannibalmusette <louise.billoud@free.fr>
2021-12-31 17:13:44 +0100

    fixed everything

---
+++ .ipynb_checkpoints/use_translator.py
@@ -0,0 +1,17 @@
+from mtranslate import translate
+
+LANGUAGES = ['af', 'pt']
+TO_TRANSLATE = "I don't know we'll see"
+
+def back_and_forth(txt, dest_lan):
+    translated_txt = translate(txt, dest_lan)
+    return translate(translated_txt)
+
+def main(txt=TO_TRANSLATE, languages=LANGUAGES):
+    if languages is None:
+        languages = LANGUAGES
+    for l in languages:
+        print(back_and_forth(txt, l))
+
+if __name__ == '__main__':
+    main()
--- data.py
+++ data.py
@@ -3,37 +3,37 @@ from datasets import Dataset, load_metric
 from typing import List
 from collections import defaultdict
 
-
 MAIN_PATH = "/media/abdelelyds/Elements/Feedback/"
 TRAIN = "train/"
 CSV_FILENAME = "train.csv.zip"
 TRANSFORMED_CSV_FILENAME = "one_single_train.csv"
 
 
-
-
-def get_raw_text(main_path: str = MAIN_PATH,
-                 folder: str = TRAIN,
-                 text_id: str = "0"):
+def get_raw_text(text_id: str,
+                 main_path: str = MAIN_PATH,
+                 folder: str = TRAIN
+                 ):
     with open(main_path + folder + f'{text_id}.txt') as f:
         return f.read()
+
 
 def read_csv_file(main_path: str = MAIN_PATH,
                   csv_filename: str = CSV_FILENAME
                   ) -> pd.DataFrame:
     return pd.read_csv(main_path + csv_filename)
 
+
 def read_transformed_csv_file(main_path: str = MAIN_PATH,
                               transformed_csv_filename: str = TRANSFORMED_CSV_FILENAME
                               ) -> pd.DataFrame:
     return pd.read_csv(main_path + transformed_csv_filename)
+
 
 def get_transformed_raw_dataset(train: pd.DataFrame,
                                 main_path: str = MAIN_PATH,
                                 filename: str = TRANSFORMED_CSV_FILENAME,
                                 save=False
                                 ) -> pd.DataFrame:
-
     df1 = train.groupby('id')['discourse_type'].apply(list).reset_index(name='classlist')
     df2 = train.groupby('id')['discourse_start'].apply(list).reset_index(name='starts')
     df3 = train.groupby('id')['discourse_end'].apply(list).reset_index(name='ends')
@@ -51,11 +51,10 @@ def get_transformed_raw_dataset(train: pd.DataFrame,
 
 
 def to_hg_dataset(df: pd.DataFrame,
-                           test_size=0.1,
-                           shuffle=True,
-                           seed=42
-                           ):
-
+                  test_size=0.1,
+                  shuffle=True,
+                  seed=42
+                  ):
     """to hugging face dataset"""
     ds = Dataset.from_pandas(df)
     return ds.train_test_split(test_size=test_size, shuffle=shuffle, seed=seed)
