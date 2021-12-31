f7c21d8231f55bf780f7d052929e917699902d33
abdel-ely-ds <abdel.ely.ds@gmail.com>
2021-12-31 18:47:40 +0100

    wip

--- labels.py
+++ labels.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import defaultdict
+import pandas as pd
 
 CLASSES = ['Lead',
  'Position',
@@ -57,8 +58,8 @@ def tokenize_and_align_labels(examples,
                   truncation=True,
                   padding=True,
                   return_offsets_mapping=True,
-                  max_length=MAX_LEN,
-                  stride=STRIDE,
+                  max_length=max_length,
+                  stride=stride,
                   return_overflowing_tokens=True)
 
     # Since one example might give us several features if it has a long context, we need a map from a feature to
@@ -96,3 +97,55 @@ def tokenize_and_align_labels(examples,
         o["labels"].append(labels)
 
     return o
+
+
+def tokenize_for_validation(examples,
+                            tokenizer,
+                            l2i,
+                            max_length):
+    o = tokenizer(examples['text'], truncation=True, return_offsets_mapping=True, max_length=max_length)
+
+    # The offset mappings will give us a map from token to character position in the original context. This will
+    # help us compute the start_positions and end_positions.
+    offset_mapping = o["offset_mapping"]
+
+    o["labels"] = []
+
+    for i in range(len(offset_mapping)):
+
+        labels = [l2i['O'] for i in range(len(o['input_ids'][i]))]
+
+        for label_start, label_end, label in \
+                list(zip(examples['starts'][i], examples['ends'][i], examples['classlist'][i])):
+            for j in range(len(labels)):
+                token_start = offset_mapping[i][j][0]
+                token_end = offset_mapping[i][j][1]
+                if token_start == label_start:
+                    labels[j] = l2i[f'B-{label}']
+                if token_start > label_start and token_end <= label_end:
+                    labels[j] = l2i[f'I-{label}']
+
+        for k, input_id in enumerate(o['input_ids'][i]):
+            if input_id in [0, 1, 2]:
+                labels[k] = -100
+
+        labels = fix_beginnings(labels)
+
+        o["labels"].append(labels)
+
+    return o
+
+
+
+def get_truth_labels(tokenized_data, train_or_test="test") -> pd.DataFrame:
+    l = []
+    for example in tokenized_data[train_or_test]:
+        for c, p in list(zip(example['classlist'], example['predictionstrings'])):
+            l.append({
+                'id': example['id'],
+                'discourse_type': c,
+                'predictionstring': p,
+            })
+
+    return pd.DataFrame(l)
+
